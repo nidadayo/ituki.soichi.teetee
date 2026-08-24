9afb4650212c83dc504ff22e02c11f8dcad19c4d
nidadayo <goonaoki0822@gmail.com>
2026-08-25 02:55:02 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,41 +1,31 @@
-
 // Hamburger
 
-document.addEventListener("DOMContentLoaded", () => {
+const hamburger = document.getElementById("hamburger");
+const nav = document.getElementById("nav");
 
-  const hamburger = document.getElementById("hamburger");
-  const nav = document.getElementById("nav");
+hamburger.addEventListener("click", () => {
+  hamburger.classList.toggle("active");
+  nav.classList.toggle("active");
+});
 
-  if (!hamburger || !nav) {
-    console.error("Hamburger or navigation not found.");
-    return;
+
+// Navigation
+
+const navLinks = document.querySelectorAll(".nav a");
+
+navLinks.forEach((link) => {
+  link.addEventListener("click", () => {
+    hamburger.classList.remove("active");
+    nav.classList.remove("active");
+  });
+});
+
+
+// ESC
+
+document.addEventListener("keydown", (event) => {
+  if (event.key === "Escape") {
+    hamburger.classList.remove("active");
+    nav.classList.remove("active");
   }
-
-  hamburger.addEventListener("click", () => {
-    hamburger.classList.toggle("active");
-    nav.classList.toggle("active");
-  });
-
-
-  // Navigation
-
-  const navLinks = nav.querySelectorAll("a");
-
-  navLinks.forEach((link) => {
-    link.addEventListener("click", () => {
-      hamburger.classList.remove("active");
-      nav.classList.remove("active");
-    });
-  });
-
-
-  // ESC
-
-  document.addEventListener("keydown", (event) => {
-    if (event.key === "Escape") {
-      hamburger.classList.remove("active");
-      nav.classList.remove("active");
-    }
-  });
-
 });
