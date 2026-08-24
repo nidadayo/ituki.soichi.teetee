3814f2ed5e542bf7fb148f029d48743f15b0c1cd
nidadayo <goonaoki0822@gmail.com>
2026-08-25 02:32:03 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,33 @@
+// Supabase
+
+const SUPABASE_URL = "https://qzwlybbjojeynhoghmyz.supabase.co/rest/v1/";
+const SUPABASE_KEY = "sb_publishable_aLU0ouME6VY_C-Yvc5SRYQ_GfSFbvmd";
+
+const supabase = window.supabase.createClient(
+  SUPABASE_URL,
+  SUPABASE_KEY
+);
+
+
+// Connection Test
+
+async function testSupabase() {
+  const { error } = await supabase
+    .from("cases")
+    .select("id")
+    .limit(1);
+
+  if (error) {
+    console.error("Supabase connection error:", error);
+    return;
+  }
+
+  console.log("Supabase connection successful!");
+}
+
+testSupabase();
+
+
 // Hamburger
 
 const hamburger = document.getElementById("hamburger");
