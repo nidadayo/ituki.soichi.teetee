119de8accffeaa45138fe600e07590dc55d2bbe2
nidadayo <goonaoki0822@gmail.com>
2026-08-25 18:14:14 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,29 @@
+// Supabase
+
+const SUPABASE_URL = "https://qzwlybbjojeynhoghmyz.supabase.co";
+
+const SUPABASE_KEY = "sb_publishable_aLU0ouME6VY_C-Yvc5SRYQ_GfSFbvmd";
+
+const supabase = window.supabase.createClient(
+  SUPABASE_URL,
+  SUPABASE_KEY
+);
+
+console.log("Supabase connected");
+
+
+// Hamburger
+
 const hamburger = document.getElementById("hamburger");
 const nav = document.getElementById("nav");
 
-hamburger.addEventListener("click", () => {
+if (hamburger && nav) {
 
-  hamburger.classList.toggle("active");
-  nav.classList.toggle("active");
+  hamburger.addEventListener("click", () => {
 
-});
+    hamburger.classList.toggle("active");
+    nav.classList.toggle("active");
+
+  });
+
+}
